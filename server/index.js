a5a618d28c9c90fe4fc5778c3afa4440eadb83d0
louis cobigo <louis.cobigo@gmail.com>
2023-05-21 20:01:34 +0200

    try fix MongoStore

--- server/index.js
+++ server/index.js
@@ -19,11 +19,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false}))
 app.use(cookieParser())
 
+const sessionStore = MongoStore.create({
+    mongoUrl: process.env.ATLAS_URI, // Remplacez par votre URL de connexion MongoDB
+    collectionName: "sessions", // Nom de la collection pour stocker les sessions
+    mongooseConnection: mongoose.connection, // Connexion Mongoose à utiliser
+  });
+  
+  
+
+// app.use(session({
+//     secret: process.env.TOKEN_SECRET,
+//     resave: false,
+//     saveUninitialized: false,
+//     store: MongoStore.create({ mongoUrl: process.env.ATLAS_URI, ttl:  60 * 100 }), //ttl: 7 days expiration
+//   }));
 app.use(session({
     secret: process.env.TOKEN_SECRET,
     resave: false,
     saveUninitialized: false,
-    store: MongoStore.create({ mongoUrl: process.env.ATLAS_URI, ttl:  60 * 100 }), //ttl: 7 days expiration
+    store: sessionStore,
+    cookie: {
+        maxAge: 24 * 60 * 60 * 1000, // Durée de validité du cookie de session (24 heures dans cet exemple)
+      }
   }));
   app.use(passport.initialize());
   app.use(passport.session());
