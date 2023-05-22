ba99198c6188fd1a3924473d642f4c53fb6d7c73
louis cobigo <louis.cobigo@gmail.com>
2023-05-23 00:59:57 +0200

    remove /session from auth context

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -23,25 +23,8 @@ export const AuthContextProvider = ({ children }) => {
     useEffect(() => {
         const sessionUser = async () => {
             const user = JSON.parse(localStorage.getItem("user"))
-            if (user === null) {
-                // const response = await fetch("https://sharify-api.vercel.app/api/user/session",{
-                //     headers: {
-                //         "Content-Type": "application/json",
-                //     },
-                //     method: 'GET'
-                // })
-                const response = await fetch("https://sharify-api.vercel.app/api/user/session", {
-                    credentials: 'include'
-                  })
-                const json = await response.json()
-                if (response.ok) {
-                    localStorage.setItem("user", JSON.stringify(json))
-                }
-            }
-            const userAuthed =JSON.parse(localStorage.getItem("user"))
-            const userConnected = user || userAuthed
-            if (userConnected ) {
-                dispatch({ type: "LOGIN", payload: userConnected })
+            if (user ) {
+                dispatch({ type: "LOGIN", payload: user })
             }
             setIsLoading(false)
         }
