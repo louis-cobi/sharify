0ba600e593ab715141c849e5ceb69d250cb5c4cf
louis cobigo <louis.cobigo@gmail.com>
2023-05-23 01:13:11 +0200

    isLoading for googleAuth

--- client/src/pages/GoogleAuth/index.jsx
+++ client/src/pages/GoogleAuth/index.jsx
@@ -4,26 +4,29 @@ import userApi from "../../api/modules/user.api"
 import { toast } from "react-toastify"
 
 const GoogleAuth = () => {
-
     const { userId } = useParams()
     const navigate = useNavigate()
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         const fetchSession = async () => {
             const { response, err } = await userApi.getSession(userId)
             if (response) {
                 localStorage.setItem("user", JSON.stringify(response))
-                navigate("/")
+                setIsLoading(false)
                 toast.success(`Welcome ${response.username}`)
             }
-            if (err){
+            if (err) {
                 toast.error(err.message)
+            }
+            if(!isLoading){
+                navigate("/")
             }
         }
         fetchSession()
-    }, [])
+    }, [isLoading])
 
-    return <div>Loading ...</div>
+    return <div>{isLoading ? "Loading..." : "Redirecting..."}</div>
 }
 
 export default GoogleAuth
