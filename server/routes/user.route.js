f6dccfee719ae551daccae448f78b535b1096670
louis cobigo <louis.cobigo@gmail.com>
2023-05-22 11:35:20 +0200

    retry session false for passport

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -49,7 +49,7 @@ router.get(
     passport.authenticate("google", {
         successRedirect: "https://sharify-app.vercel.app/",
         failureRedirect: "https://sharify-app.vercel.app/login",
-        // session: false
+        session: false
     })
 )
 
