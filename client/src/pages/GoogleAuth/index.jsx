d53e250b76c092e8bf5ff97762390b7b0bbcaea8
louis cobigo <louis.cobigo@gmail.com>
2023-05-23 01:23:54 +0200

    add useAuthContext

--- client/src/pages/GoogleAuth/index.jsx
+++ client/src/pages/GoogleAuth/index.jsx
@@ -1,32 +1,31 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { useParams, useNavigate } from "react-router-dom"
+import { useAuthContext } from "./useAuthContext"
 import userApi from "../../api/modules/user.api"
 import { toast } from "react-toastify"
 
 const GoogleAuth = () => {
     const { userId } = useParams()
     const navigate = useNavigate()
-    const [isLoading, setIsLoading] = useState(true)
+    const { dispatch } = useAuthContext()
 
     useEffect(() => {
         const fetchSession = async () => {
             const { response, err } = await userApi.getSession(userId)
             if (response) {
                 localStorage.setItem("user", JSON.stringify(response))
+                dispatch({ type: "LOGIN", payload: response })
                 toast.success(`Welcome ${response.username}`)
-                setIsLoading(false)
+                navigate("/")
             }
             if (err) {
                 toast.error(err.message)
             }
-            if(!isLoading){
-                navigate("/")
-            }
         }
         fetchSession()
-    }, [isLoading])
+    }, [])
 
-    return <div>{isLoading ? "Loading..." : "Redirecting..."}</div>
+    return <div> "Loading..."</div>
 }
 
 export default GoogleAuth
